03366be6daf612066040cd343ca52df3a1131d64
aryan13072001 <aryangupta13july>
2024-01-24 17:52:56 +0530

    updated

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,12 +1,12 @@
 // Immediately Invoked Function Expressions (IIFE)
-
+//used to avoid pollutioning that maybe caused by the variables present in the global scope 
 
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})();  //this semi-colon is important to mark the end of a particular IIFE scope 
 
-( (name) => {
+( (name) => {  
     console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+} )('hitesh')  //passing arguments 
 
